dbfb3a6c4fad7660f36e7385b2c624f893375cf7
RodrigoLCA <rodrigolca@gmail.com>
2025-01-11 13:21:32 -0300

    Criação e implementação dos métodos de manipulação da rota de User

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,46 +1,46 @@
 // import Star from "../models/star.model";
+import Star from "../models/star.model.js";
 import User from "../models/user.model.js";
 
-// apenas para teste e criação da collection
-export const NovoUsuario = async(req, res) => {
-    try {
+// // criação de um novo usuário.
+// export const NovoUsuario = async(req, res) => {
+//     try {
 
-        const {nome, email, ativo} = req.body;
+//         const {nome, email, ativo} = req.body;
 
-        if( !nome || !email || !ativo) {
-            return res.status(400).json({
-                success: false,
-                message: "É necessário nome, email e atividade."
-            });
-        }
+//         if( !nome || !email || !ativo) {
+//             return res.status(400).json({
+//                 success: false,
+//                 message: "É necessário nome, email e atividade."
+//             });
+//         }
 
-        const novoUsuario = new User({nome, email, ativo});
+//         const novoUsuario = new User({nome, email, ativo});
 
-        await novoUsuario.save();
+//         await novoUsuario.save();
 
-        res.status(200).json({
-            success: true,
-            message: "Novo usuário adicionado com sucesso!"
-        });
-    } catch (erro) {
-        res.status(500).json({
-            success: false,
-            message: "Erro no servidor.",
-            erro
-        });
-    }
-
-}
+//         res.status(200).json({
+//             success: true,
+//             message: "Novo usuário adicionado com sucesso!"
+//         });
+//     } catch (erro) {
+//         res.status(500).json({
+//             success: false,
+//             message: "Erro no servidor.",
+//             erro
+//         });
+//     }
+// }
 
 // teremos /usuario:id
 export const ObterUsuario = async(req, res) => {
     const { id } = req.params;
 
-    console.log(`Comando de obtenção de um usuario: id ${id}`)
-
     try {
-        const usuario = await User.findById(id)
-console.log(usuario);
+
+        const usuario = await User.findById(id);
+
+        // não encontrou? retorna erro 400.
         if(!usuario) {
             return res.status(400).json({
                 success: false,
@@ -48,28 +48,66 @@ console.log(usuario);
             });
         }
 
+        // caso contrário, retorna o valor do db.
         res.status(200).json(usuario);
+
     } catch (error) {
+
         res.status(500).json({
             success: false,
             message: "Erro no servidor.",
             error
         });
+
     }
 }
 
 // teremos /usuario:id/stars/:astro_id
 export const ObterAstro = async(req, res) => {
+    /**
+     * Esse bloco de código está em construção.
+     * Motivo: precisaremos do SCHEMA de aquisições.
+     * Pois não estaremos obtendo do schema de STAR,
+     * Pois o Schema de STAR terá apenas as informações
+     * GERAIS das figurinhas. Para informações detalhadas
+     * Como a mensagem, a foto, etc. Será puxado do schema
+     * de aquisição do usuário, que é onde vai ficar essas
+     * informações, de acordo com os diagramas.
+     */
     const { id, astro_id } = req.params;
 
-    try {
-        // aqui teremos a interação com o modelo
-        // de astros.
-        res.status(400).json({
+    if(!id) {
+        return res.status(400).json({
             success: false,
-            message: "Funcionalidade em processo...",
+            message: "É necessário ID do usuario."
+        });
+    }
+
+    try {
+
+        /**
+         * let criteria = {id_usuario: id};
+         * 
+         * if(astro_id)
+         *  criteria.id_astro = astro_id;
+         * 
+         * const purchase = await Purchase.find(criteria);
+         * 
+         * res.status(200).json({
+         *  success: true,
+         *  user_id: id,
+         *  purchase
+         * });
+         */
+
+
+        res.status(200).json({
+            success: true,
+            message: "Implementação em andamento...",
             usuario_id: id,
-        })
+            purchase: {},
+        });
+
     } catch( error ) {
         res.status(500).json({
             success: false,
@@ -78,37 +116,72 @@ export const ObterAstro = async(req, res) => {
     }
 }
 
-// teremos /usuario:id/bloquear
-export const Bloquear = async(req, res) => {
-
-    const { id } = req.params;
-
+/**
+ * Este método trata as rotas de user/block e user/unblock
+ * ADMIN APENAS
+ * serve para definir se o usuário estará com o login
+ * ativo ou não.
+ * 
+ * Isso poderá futuramente definir a visibilidade dos
+ * astro que este usuário comprou, por questões de
+ * termos de uso e legalidade.
+ */
+export const UserSetBlock = async(req, res) => {
     try {
-        const bloq_usuario = await User.findByIdAndUpdate(
-            id,
-            { ativo: false },
-            {new: true } // Retorna o documento atualizado
-        );
 
-        if(!bloq_usuario) {
-            return res.status(404).json({
+        const user_id = req.params.id;
+
+        if(!user_id) {
+            res.status(400).json({
                 success: false,
-                message: 'Usuario não encontrado, admin.'
+                message: "ID do usuário é necessário."
             });
         }
 
+        /**
+         * Ajustaremos o updatedUser e setUserActive
+         * 
+         * updatedUser: o valor retornado pelo Update();
+         * 
+         * setUserActive: valor definido como true, que
+         *                mudará de acordo com a rota
+         *                identificada.
+         */
+        let updatedUser = {};
+        let setUserActive = true;
+
+        // rota de block? então setUserActive = false.
+        if(req.path.includes("/block"))
+            setUserActive = false;
+
+        // aqui definir o update genérico, e daremos o
+        // status de ativo ou não com base no resultado anterior.
+        updatedUser = await User.findByIdAndUpdate(
+            user_id,
+            { ativo: setUserActive },
+            { new: true } // retorna o documento atualizado
+        );
+
+        // retornaremos sucesso e o objeto atualizado.
         res.status(200).json({
             success: true,
-            message: 'Usuario bloqueado com sucesso.'
+            updatedUser,
         });
-    } catch (erro) {
+
+    } catch ( error ) {
+
+        // qualquer erro, retorna erro.
         res.status(500).json({
             success: false,
-            message: 'Erro no servidor.'
+            message: "Erro no servidor.",
+            error
         });
     }
 }
 
+/**
+ * Criação de um novo User.
+ */
 export const Novo = async(req, res) => {
     try {
         const { nome, email, ativo } = req.body;
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,22 @@
 import express from "express";
-import { NovoUsuario, ObterAstro, ObterUsuario } from "../controllers/user.controller.js";
+import { ObterAstro, ObterUsuario, UserSetBlock } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 // POST /users
 // teste apenas.
-router.post('/', NovoUsuario);
+//router.post('/', NovoUsuario);
+
+// POST /users/block
+// Rota para bloquear usuario
+// (ADMIN APENAS)
+router.post('/:id/block', UserSetBlock);
+
+// em breve /users/unblock
+// de acordo com API (aguardando sprint)
+// será direcionado para a mesma rota.
+// o tratamento será feito no método UserSetBlock
+// (ADMIN APENAS)
 
 // GET /users:id
 // Obteremos informações sobre o usuario específico
@@ -17,6 +28,6 @@ router.get('/:id/stars', ObterAstro);
 
 // GET /users/:id/stars/:id
 // Obteremos info de um astro do usuário específico.
-router.get('/:id/stars:astro_id', ObterAstro);
+router.get('/:id/stars/:astro_id', ObterAstro);
 
 export default router;
